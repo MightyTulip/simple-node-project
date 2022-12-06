c9d1708abd688cacfdeba3cb2373c03d9ff37c7a
MightyTulip <eddy.chou89@gmail.com>
2022-12-06 13:55:37 +1100

    Full interactive node page

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const https = require('http')
 const fs = require('fs')
+require('dotenv').config()
 
-const hostname = '127.0.0.1';
-const port = 3000;
 const urlMap = {
     '/': './index.html',
     '/about': './about.html',
@@ -13,16 +12,12 @@ const server = https.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   try {
-    console.log('try', req.url)
-    fs.readFile(urlMap[req.url], (err, data) => {
-        res.end(data)
-    })
+    fs.readFile(urlMap[req.url], (err, data) => res.end(data))
   } catch (error) {
-    console.log('error')
     fs.readFile('./404.html', (err, data) => res.end(data))
   }
-});
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(process.env.port, process.env.hostname, () => {
+    console.log(`Server running at http://${process.env.hostname}:${process.env.port}/`);
+})
