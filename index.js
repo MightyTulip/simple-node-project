5091e9352c1e4cc64aaa20550efc13b063df7ba4
MightyTulip <eddy.chou89@gmail.com>
2022-12-15 11:00:26 +1100

    Update to use expressJS

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-const https = require('http')
-const fs = require('fs')
-require('dotenv').config()
+const express = require('express')
+const path = require('path')
+// Only for local environment
+// require('dotenv').config()
 
 const urlMap = {
-    '/': './index.html',
-    '/about': './about.html',
-    '/contact-me': './contact-me.html'
+    '/': '/index.html',
+    '/about': '/about.html',
+    '/contact-me': '/contact-me.html'
 }
 
-const server = https.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  try {
-    fs.readFile(urlMap[req.url], (err, data) => res.end(data))
-  } catch (error) {
-    fs.readFile('./404.html', (err, data) => res.end(data))
-  }
+const app = express().get('*', (req, res) => {
+  const map = urlMap[req.path] || '404.html'
+  res.sendFile(path.join(__dirname, map));
 })
 
-server.listen()
+app.listen()
 
 // Use this to test on local
-// server.listen(process.env.port, process.env.hostname, () => {
+// app.listen(process.env.port, process.env.hostname, () => {
 //     console.log(`Server running at http://${process.env.hostname}:${process.env.port}/`);
 // })
